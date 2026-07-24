52661bb4852c00a1235cf772de666b84b5d8263b
vasiliki-p <127341252+vasiliki-p@users.noreply.github.com>
2026-07-25 01:07:50 +0300

    Update my_requests.php

--- my_requests.php
+++ my_requests.php
@@ -1,12 +1,11 @@
 <?php
-<<<<<<< HEAD
 include 'user_id login.php';
 include 'connection.php';
 include 'myrequests css.html';
 
 // Εκτέλεση ερωτήματος για την ανάκτηση λεπτομερειών των αιτημάτων του χρήστη
 $stmt = $conn->prepare("SELECT id, title, request FROM requests WHERE user_id = ? ORDER BY created_at DESC");
-=======
+
 session_start();
 include 'myrequests css.html';
 
@@ -35,7 +34,6 @@ if ($conn->connect_error) {
 
 // Εκτέλεση ερωτήματος για την ανάκτηση λεπτομερειών των αιτημάτων του χρήστη
 $stmt = $conn->prepare("SELECT id, title, request FROM requests WHERE user_id = ?");
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
 $stmt->bind_param("i", $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
@@ -45,17 +43,15 @@ echo "<h1>Τα Αιτήματά μου</h1>";
 // Έλεγχος αποτελεσμάτων
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-<<<<<<< HEAD
         echo "<h2><a href='answer.php?id=" . $row["id"] . "'>" . $row["title"] . "</a></h2>";
     }
 } else {
     echo "Δεν βρέθηκαν αιτήματα ";
-=======
+
         echo "<h2><a href='request.php?id=" . $row["id"] . "'>" . $row["title"] . "</a></h2>";
     }
 } else {
     echo "Δεν βρέθηκε αίτημα για τον χρήστη με ID: " . $user_id;
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
 }
 echo "</div>";
 
