559def2b85c4e5b8b56f480efaf1e6fcb8442697
vasiliki-p <127341252+vasiliki-p@users.noreply.github.com>
2026-07-24 23:57:45 +0300

    Resolve merge conflicts in answer.php

--- answer.php
+++ answer.php
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 
 <?php
 include 'user_id login.php';
 include 'connection.php';
 include 'answer css.html';
 
-=======
 <?php
 session_start();
 include 'answer css.html';
@@ -35,7 +33,6 @@ if ($conn->connect_error) {
 //echo "Connected successfully";
 
 
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
 ?>
 
 <!DOCTYPE html>
@@ -45,16 +42,13 @@ if ($conn->connect_error) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <<<<<<< HEAD
-=======
     <!-- Include your CSS file -->
        <link rel="stylesheet" type="text/css" href="answers_form css.html">
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
 
 </head>
 <body>
 
     <div class="answer_form">
-<<<<<<< HEAD
         <!--<h1>Λεπτομέρειες Απάντησης</h1>-->
 
 
@@ -75,7 +69,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
            echo "<script>window.location.href = 'my_requests.php';</script>";
             // Μεταφορά πίσω στη λίστα των αιτημάτων
             exit();
-=======
         <h1><strong>Λεπτομέρειες Απάντησης</strong></h1>
 
 
@@ -93,7 +86,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if ($stmt->execute()) {
             echo "Η απάντηση καταχωρήθηκε επιτυχώς.";
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
         } else {
             echo "Συνέβη κάποιο σφάλμα κατά την καταχώρηση της απάντησης.";
         }
@@ -102,32 +94,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Δε βρέθηκαν απαραίτητα πεδία.";
     }
  }
-<<<<<<< HEAD
  
-=======
-
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
         if (isset($_GET['id'])) {
             $id = $_GET['id'];
 
             // Query to select details of the request
-<<<<<<< HEAD
             $stmt = $conn->prepare("SELECT requests.id AS request_id, requests.title, requests.request, requests.created_at,requests.name, concat( answers.user_answer,' ', answers.admin_answer)  as answers, users.username
             FROM requests  
             LEFT JOIN answers ON requests.id = answers.request_id 
             LEFT JOIN users ON users.user_id = answers.user_id
             WHERE requests.id = ? 
             ");
-=======
             $stmt = $conn->prepare("SELECT id, title, request, answer  FROM requests WHERE id = ? AND answer!='' ");
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
             $stmt->bind_param("i", $id);
             $stmt->execute();
             $result = $stmt->get_result();
             $row = $result->fetch_assoc();
 
             if ($result->num_rows > 0) {
-<<<<<<< HEAD
                 echo "<div class='container'>";
                 echo "<div class='header-box'>";  
                 echo "<span style='margin-right: 20px;'><strong>Θέμα:</strong> " . $row['title'] . "</span>";
@@ -162,7 +146,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo "Δεν βρέθηκε αίτημα με το συγκεκριμένο ID.";
             }
         
-=======
                 echo "<p><strong>Θέμα:</strong> " . $row['title'] . "</p>";
                 echo "<p><strong>Αίτημα:</strong> " . $row['request'] . "</p>";
                 echo "<p><strong>Απάντηση:</strong> " . $row['answer'] . "</p>";
@@ -171,16 +154,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 echo "Δεν βρέθηκε αίτημα με το συγκεκριμένο ID.";
             }
 
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
             $stmt->close();
         } else {
             echo "Δεν βρέθηκαν απαντήσεις.";
         }
-<<<<<<< HEAD
-        
-=======
-     
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
        // Close the connection
 
 $conn->close();
@@ -188,7 +165,6 @@ $conn->close();
     </div>
 </body>
 </html>
-<<<<<<< HEAD
 
 =======
 >>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
