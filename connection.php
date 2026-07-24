ffdf00b0a5913838985e43c621f3ebaed4cf95c9
vasiliki-p <127341252+vasiliki-p@users.noreply.github.com>
2026-07-24 19:12:54 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
 <?php
+/*
 $servername = "localhost";
 $username = "admin";
 $password = "B@kal@r05";
@@ -11,5 +12,37 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully";
+//echo "Connected successfully";*/
+?>
+
+
+<?php
+// Παίρνουμε το URL από το Render, αν υπάρχει
+$db_url = getenv("DATABASE_URL");
+
+if ($db_url) {
+    // Είμαστε στο Render: "Σπάμε" το URL στα κομμάτια του
+    $dbparts = parse_url($db_url);
+    
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'], '/');
+    $port = $dbparts['port'];
+} else {
+    // Είμαστε τοπικά (XAMPP/WAMP): Βάλε εδώ τα παλιά σου στοιχεία
+$servername = "localhost";
+$username = "admin";
+$password = "B@kal@r05";
+$dbname = "register";
+   $port = 3306;
+}
+
+// Δημιουργία σύνδεσης
+$conn = new mysqli($hostname, $username, $password, $database, $port);
+
+// Έλεγχος σύνδεσης
+if ($conn->connect_error) {
+    die("Η σύνδεση απέτυχε: " . $conn->connect_error);
+}
 ?>
