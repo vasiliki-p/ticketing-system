e535f238eb6e0613bbe52fb038f0b570a2767a3b
vasiliki-p <127341252+vasiliki-p@users.noreply.github.com>
2026-07-25 02:08:39 +0300

    Update user.php

--- user.php
+++ user.php
@@ -1,18 +1,6 @@
 <?php 
 include 'user_id login.php';
 include 'connection.php';
-
-// Έλεγχος αν ο χρήστης έχει συνδεθεί
-if (!isset($_SESSION['user_id'])) {
-    // Αν δεν έχει συνδεθεί, ανακατεύθυνση στη σελίδα σύνδεσης
-    header("Location: login.php");
-    exit();
-}
-
-// Αν έχει συνδεθεί, μπορείτε να χρησιμοποιήσετε το user_id από τη συνεδρία
-$user_id = $_SESSION['user_id'];
-
-// Τώρα μπορείτε να χρησιμοποιήσετε το $user_id για να ανακτήσετε τα αιτήματά του από τη βάση δεδομένων
 ?>
 
 <!DOCTYPE html>
@@ -23,7 +11,6 @@ $user_id = $_SESSION['user_id'];
  
         <title>Home Page</title>
 
-        <title>Welcome Page</title>
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.17/dist/tailwind.min.css" rel="stylesheet">
     </head>
     <body>
