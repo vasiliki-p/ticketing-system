96e234bafd6ad4ec36874ab5b87ead1d16108309
vasiliki-p <127341252+vasiliki-p@users.noreply.github.com>
2026-07-25 01:07:07 +0300

    Refactor database query and remove connection code
    Removed database connection code and updated SQL query to include 'request' and 'answer' fields.

--- my_answers.php
+++ my_answers.php
@@ -12,32 +12,16 @@ if (!isset($_SESSION['user_id'])) {
 // Αν έχει συνδεθεί, μπορείτε να χρησιμοποιήσετε το user_id από τη συνεδρία
 $user_id = $_SESSION['user_id'];
 
-// Εκτέλεση σύνδεσης με τη βάση δεδομένων
-$servername = "localhost";
-$username = "admin";
-$password = "B@kal@r05";
-$dbname = "register";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Έλεγχος σύνδεσης
-if ($conn->connect_error) {
-    die("Αποτυχία σύνδεσης: " . $conn->connect_error);
-}
-
 // Εκτέλεση ερωτήματος για την ανάκτηση λεπτομερειών των αιτημάτων του χρήστη
-<<<<<<< HEAD
 $stmt = $conn->prepare("SELECT id, title FROM requests WHERE user_id = ? AND request != '' ORDER BY created_at DESC");
 $stmt->bind_param("i", $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
 
-=======
 $stmt = $conn->prepare("SELECT id, title, request, answer FROM requests WHERE user_id = ? AND answer!='' ORDER BY created_at DESC");
 $stmt->bind_param("i", $user_id);
 $stmt->execute();
 $result = $stmt->get_result();
->>>>>>> fdcf3ac0bc7103ec101333d572ddae28c826284b
 echo "<div class='reg-form'>";
 echo "<h1> Απαντήσεις</h1>";
 // Έλεγχος αποτελεσμάτων
