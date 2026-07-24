4f1d02fb33c40aa050ceb2ca8b9dbb885f614486
vasiliki-p <127341252+vasiliki-p@users.noreply.github.com>
2026-07-25 01:38:37 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -4,21 +4,3 @@ include 'admin_menu.html';
 include 'connection.php';
 
 ?>
-
-include 'admin_menu.html';
-
-$servername = "localhost";
-$username = "admin";
-$password = "B@kal@r05";
-$dbname = "register";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-//echo "Connected successfully";
-?>
-
