f8222eb7a72dacba3200e60761b8dcb9c23ce201
vasiliki-p <127341252+vasiliki-p@users.noreply.github.com>
2026-07-27 18:57:52 +0300

    Αρχικό commit

---
+++ connection.php
@@ -0,0 +1,30 @@
+<?php
+// Παίρνουμε το URL από το Render, αν υπάρχει
+$db_url = getenv("DATABASE_URL");
+
+if ($db_url) {
+    // Είμαστε στο Render: "Σπάμε" το URL στα κομμάτια του
+    $dbparts = parse_url($db_url);
+    
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'], '/');
+    $port = $dbparts['port'];
+} else {
+    // Είμαστε τοπικά (XAMPP/WAMP): Τα ονόματα των μεταβλητών διορθώθηκαν
+    $hostname = "localhost";
+    $username = "admin";
+    $password = "B@kal@r05";
+    $database = "register";
+    $port = 3306;
+}
+
+// Δημιουργία σύνδεσης
+$conn = new mysqli($hostname, $username, $password, $database, $port);
+
+// Έλεγχος σύνδεσης
+if ($conn->connect_error) {
+    die("Η σύνδεση απέτυχε: " . $conn->connect_error);
+}
+?>
